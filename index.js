843bf9229c9ba6dbdb89ae8e14b1d5a0c3ed3b05
jpcreativeworks <jp.graphics.011@gmail.com>
2022-04-13 16:06:24 -0700

    corrected no mod err and updated questions and index name

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const inquirer = require ("inquirer");
-const fs = require ("fs");
+const inquirer = require("inquirer");
+const fs = require("fs");
 const Engineer = require("./library/Engineer");
 const Intern = require("./library/Intern");
 const Manager = require("./library/Manager");
+let managerHTML = "";
+let internHTML = "";
+let engieerHTML = "";
 
-function requestingManager(){
+function requestingManager() {
     inquirer.prompt([
         {
             type: 'input',
@@ -26,15 +29,24 @@ function requestingManager(){
             message: 'Enter your office number',
             name: 'managerOfficeNum'
         },
-    ]) .then(function(managerInput){
+    ]).then(function (managerInput) {
         const newManager = new Manager(
             managerInput.managerName,
-            managerInput.managerID,
+            managerInput.managerId,
             managerInput.managerEmail,
             managerInput.managerOfficeNum
-        ); 
+        );
+
+        managerHTML = `<div class="card border-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header">${newManager.name}</div>
+        <div class="card-body text-primary">
+          <h5 class="card-title">${newManager.getRole()}</h5>
+          <p class="card-text">Id: ${newManager.id} <br> Email: ${newManager.email} <br> Office:  ${newManager.officeNum}</p>
+        </div>
+      </div>`
         console.log(newManager);
-    })
+        requestEmployee()
+    });
 }
 
 function requestEmployee() {
@@ -45,9 +57,11 @@ function requestEmployee() {
             choices: ['Engineer', 'Intern', 'Exit'],
             name: 'employeeChoice',
 
-        },
-    ]) .then(function(employeeInput){
-            switch(employeeInput.employeeChoice){
+        }
+
+    ])
+        .then(function (employeeInput) {
+            switch (employeeInput.employeeChoice) {
                 case "Engineer":
                     addNewEngineer();
                     break;
@@ -57,7 +71,97 @@ function requestEmployee() {
                 default:
                     addExit();
                     break;
-            };
-            
-    })
+            }
+
+        })
 }
+
+requestingManager();
+
+
+function addNewEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter your name',
+            name: 'engineerName'
+        },
+        {
+            type: 'input',
+            message: 'Enter your ID Number',
+            name: 'engineerId'
+        },
+        {
+            type: 'input',
+            message: 'Enter your email',
+            name: 'engineerEmail'
+        },
+        {
+            type: 'input',
+            message: 'what is your gitHub link?',
+            name: 'engineerGitHub',
+        },
+    ]).then(function (engineerInput) {
+        const newEngineer = new Engineer(
+            engineerInput.engineerName,
+            engineerInput.engineerId,
+            engineerInput.engineerEmail,
+            engineerInput.engineerGitHub
+        );
+
+        engineerHTML = `<div class="card border-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header">${newEngineer.name}</div>
+        <div class="card-body text-primary">
+          <h5 class="card-title">${newEngineer.getRole()}</h5>
+          <p class="card-text">Id: ${newEngineer.id} <br> Email: ${newEngineer.email} <br> Office:  ${newEngineer.GitHub}</p>
+        </div>
+      </div>`
+        console.log(newEngineer);
+        requestEmployee()
+    });
+}
+function addNewIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter your name',
+            name: 'internName'
+        },
+        {
+            type: 'input',
+            message: 'Enter your ID Number',
+            name: 'internId'
+        },
+        {
+            type: 'input',
+            message: 'Enter your email',
+            name: 'internEmail'
+        },
+        {
+            type: 'input',
+            message: 'where did you go to school?',
+            name: 'internSchool',
+        },
+    ]).then(function (internInput) {
+        const newIntern = new Intern(
+            internInput.internName,
+            internInput.internId,
+            internInput.internEmail,
+            internInput.internSchool
+        );
+
+        internHTML = `<div class="card border-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header">${newIntern.name}</div>
+        <div class="card-body text-primary">
+          <h5 class="card-title">${newIntern.getRole()}</h5>
+          <p class="card-text">Id: ${newIntern.id} <br> Email: ${newIntern.email} <br> Office:  ${newIntern.school}</p>
+        </div>
+      </div>`
+        console.log(newIntern);
+        requestEmployee()
+    });
+}
+
+function addExit() {
+    console.log('finished!')
+};
