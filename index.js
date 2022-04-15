64d50ea333d58835449129051cb7a10f8c2bf8bd
jpcreativeworks <jp.graphics.011@gmail.com>
2022-04-15 10:03:25 -0700

    updated bootstrap styling

--- index.js
+++ index.js
@@ -150,7 +150,8 @@ function addNewIntern() {
             internInput.internSchool
         );
 
-        internHTML += `<div class="card border-primary mb-3" style="max-width: 18rem;">
+        internHTML += `
+        <div class="card border-primary mb-3" style="max-width: 18rem;">
         <div class="card-header">${newIntern.name}</div>
         <div class="card-body text-primary">
           <h5 class="card-title">${newIntern.getRole()}</h5>
@@ -172,36 +173,42 @@ function addExit() {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="./assets/style.css" rel="stylesheet">
     <title>Join Our Team</title>
 </head>
 <body>
-    <header class="class navBar"> Join our team!</header>
-        <ul class="nav justify-content-end">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#cards">Home</a>
-            </li>
-            
-            <li class="nav-item"> 
-                <a class="nav-link active" aria-current="page" href="#instructions">Instructions</a>
-            </li>
-        </ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container-fluid">
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#">Join our team!</a>
+    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#cards">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#instructions">Instructions</a>
+      </li>                
+    </ul>
+  </div>
+</div>
+</nav>
+
+<div id="cards" class="row">
+          <div class="column mb-3">
+              <div class="class card"></div>
+          </div>
+</div>
     
-    <main class="container">
+    <main class="container bg-light">
     ${managerHTML}
     ${internHTML}
-    ${engineerHTML}
-
-        <div id="cards" class="row">
-            <div class="column">
-                <div class="class card"></div>
-                <div class="class card"></div>
-                <div class="class card"></div>
-            </div>
-        </div>
-       
+    ${engineerHTML}               
     </main>
     <footer> <p id="instructions">
-    this is where some instructions will go. 
+    Thanks for using Join Our Team! Use the prompts in running index.js from <a href="https://github.com/jpcreativeworks">GitHub</a>. If you have more questions, please feel free to reach out. 
     </p>
 </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
